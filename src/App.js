304eb96874b8b37614d26c9824c828de5d9a1ce4
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-05-03 10:15:04 -0300

    You Rock

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+
+import params from './params'
+import { createMineBoard, cloneBoard, openField, hasExplosion, wonGame, showMines, invertFlag, flagsUsed } from './functions'
+
+import MineField from './components/MineField'
+import Header from './components/Header'
+
+import LevelSelection from './screens/LevelSelection'
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
+      showLevelSelection: false,
+    }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hasExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert(':(','You Loooooser!!!')
+    }
+
+    if (won) {
+      Alert.alert(':D','You Rock \m/')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board, row, column)
+    const won = wonGame(board)
+
+    if (won) {
+      Alert.alert(':D','You Rock \m/')
+    }
+
+    this.setState({ board, won })
+
+  }
+
+  onLevelSelected = level => {
+    params.difficultLevel = level
+    this.setState(this.createState())
+  }
+
+  render() {
+      return (
+        <View style={styles.container}>
+          <LevelSelection isVisible={this.state.showLevelSelection}
+            onLevelSelected={this.onLevelSelected}
+            onCancel={() => this.setState({ showLevelSelection: false })} />
+          <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)}
+            onNewGame={() => this.setState(this.createState())}
+            onFlagPress={() => this.setState({ showLevelSelection: true })} />
+          <View style={styles.board}>
+            <MineField board={this.state.board}
+              onOpenField={this.onOpenField}
+              onSelectField={this.onSelectField} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+});
